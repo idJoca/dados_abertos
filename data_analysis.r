2de2b09cc8081288c4f6daefa1e5faf95eddefd1
idJoca <37230465+idJoca@users.noreply.github.com>
2019-08-15 20:42:56 -0300

    Added labels & saved charts

--- data_analysis.r
+++ data_analysis.r
@@ -3,8 +3,9 @@ install.packages("gplots")
 library("gplots")
 library("plot3D")
 library("plot3Drgl")
-propostas_deputados = read.csv("legislatura_todas/propostas.csv")
-gastos_deputados = read.csv("legislatura_todas/gastos.csv")
+getwd()
+propostas_deputados = read.csv("../../../dados_abertos-master/legislatura_todas/propostas.csv")
+gastos_deputados = read.csv("../../../dados_abertos-master/legislatura_todas/gastos.csv")
 rowCondition <- apply(gastos_deputados[,-1],1,function(x) any(x[-length(x)] == 'Total'))
 gastos <- gastos_deputados[rowCondition,]
 sums.year <- aggregate(as.double(gastos$Valor),gastos["Ano"],sum)
@@ -26,13 +27,19 @@ fitpoints <- predict(fit)
 scatter3D(x, y, z, surf = list(x = x.pred, y = y.pred, z = z.pred,  
     facets = NA, fit = fitpoints))
 scatter3D(x, y, z,
-          theta = 180, phi = -70, pch = 18, type='p', bty='b2', colvar=z)
+          theta = 90, phi = -60, pch = 18, type='p', bty='b2',
+	    colvar=z, xlab="Nr. Propostas", ylab="Gastos", zlab="Anos")
 scatter2D(x, y, type='h')
 scatter2D(party.cost$x, party.propostas$x)
-text2D(party.propostas$x, party.cost$x, party.cost$Partido)
-abline(lm(party.cost$x ~ party.propostas$x))
+text2D(party.propostas$x, party.cost$x, party.cost$Partido,
+	 colvar=party.propostas$x,
+	 xlab="Nr. Propostas", ylab="Gastos Partido")
+abline(lm(party.cost$x ~ party.propostas$x), col="green")
+abline(h=mean(party.cost$x), col="blue")
+abline(v=mean(party.propostas$x), col="red")
 hist3D(z = table(party.propostas$x, party.cost$x))
 hist(party.cost$x)
-hist3D(z = table(propostas_deputados$Total, as.numeric(propostas_deputados$Ano)))
+hist3D(z = table(propostas_deputados$Total, as.numeric(propostas_deputados$Ano)),
+	 theta = 90, phi = 60)
 heatmap(table(propostas_deputados$Ano, propostas_deputados$Total), scale = "column")
 plotrgl()
