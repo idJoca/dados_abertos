87d4a039b5a749cb733220dc72f68537696a08a9
idJoca <37230465+idJoca@users.noreply.github.com>
2019-08-17 13:00:13 -0300

    new charts

--- data_analysis.r
+++ data_analysis.r
@@ -1,11 +1,15 @@
 install.packages("plot3Drgl")
 install.packages("gplots")
+install.packages("abjutils")
+
+library(abjutils)
 library("gplots")
 library("plot3D")
 library("plot3Drgl")
 getwd()
-propostas_deputados = read.csv("legislatura_todas/propostas.csv")
-gastos_deputados = read.csv("legislatura_todas/gastos.csv")
+propostas_deputados = read.csv("../../../dados_abertos-master/legislatura_todas/propostas.csv")
+
+gastos_deputados = read.csv("../../../dados_abertos-master/legislatura_todas/gastos.csv")
 rowCondition <- apply(gastos_deputados[,-1],1,function(x) any(x[-length(x)] == 'Total'))
 single_year <- apply(gastos_deputados,1,function(x) x["Ano"] == 2009)
 gastos <- gastos_deputados[rowCondition,]
@@ -34,6 +38,11 @@ text2D(party.propostas$x, party.cost$x, party.cost$Partido,
 	 colvar=party.propostas$x,
 	 xlab="Nr. Propostas", ylab="Gastos Partido",
      cex=0.7)
+text3D(x, y, z, rm_accent(propostas_deputados$Nome), cex=0.5,
+	          theta = 90, phi = -60, pch = 18, type='p', bty='b2',
+	    colvar=z, xlab="Nr. Propostas", ylab="Gastos", zlab="Anos")
+ plotdev(xlim = c(0, 10), ylim = c(40, 150), 
+         zlim = c(7, 25))
 abline(lm(party.cost$x ~ party.propostas$x), col="green")
 abline(h=mean(party.cost$x), col="blue")
 abline(v=mean(party.propostas$x), col="red")
